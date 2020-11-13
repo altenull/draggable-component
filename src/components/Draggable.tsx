501e1ed86e3b808acbfb331da554e86c23568c51
Heonyoung Kim <altenull@gmail.com>
2020-11-14 04:09:14 +0900

    feat: add or remove mouse event listener on Draggable depending on box dragging whether box is being dragged or not

--- src/components/Draggable.tsx
+++ src/components/Draggable.tsx
@@ -1,11 +1,55 @@
-import React, { isValidElement, ReactChild, useRef } from "react";
+import React, {
+  isValidElement,
+  ReactChild,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
 interface Props {
   children: ReactChild;
 }
 
 const Draggable: React.FC<Props> = ({ children }) => {
+  const [isBoxDragging, setIsBoxDragging] = useState<boolean>(false);
+
+  const draggableRef = useRef<HTMLDivElement>(null);
   const boxRef = useRef<HTMLDivElement>(null);
+
+  const dragStart = (event: MouseEvent) => {
+    if (event.target === boxRef.current) {
+      setIsBoxDragging(true);
+    }
+  };
+
+  const dragEnd = (event: MouseEvent) => {
+    setIsBoxDragging(false);
+  };
+
+  const drag = (event: MouseEvent) => {};
+
+  useEffect(() => {
+    draggableRef?.current?.addEventListener("mousedown", dragStart);
+
+    return () => {
+      draggableRef?.current?.removeEventListener("mousedown", dragStart);
+    };
+  }, [draggableRef.current]);
+
+  useEffect(() => {
+    if (isBoxDragging) {
+      draggableRef?.current?.addEventListener("mouseup", dragEnd);
+      draggableRef?.current?.addEventListener("mousemove", drag);
+    } else {
+      draggableRef?.current?.removeEventListener("mouseup", dragEnd);
+      draggableRef?.current?.removeEventListener("mousemove", drag);
+    }
+
+    return () => {
+      draggableRef?.current?.removeEventListener("mouseup", dragEnd);
+      draggableRef?.current?.removeEventListener("mousemove", drag);
+    };
+  }, [draggableRef.current, isBoxDragging]);
 
   const draggableStyle = {
     display: "flex",
@@ -18,7 +62,7 @@ const Draggable: React.FC<Props> = ({ children }) => {
   };
 
   return (
-    <div style={draggableStyle}>
+    <div style={draggableStyle} ref={draggableRef}>
       {React.Children.map(children, (child) => {
         if (isValidElement(child)) {
           return React.cloneElement(child, {
